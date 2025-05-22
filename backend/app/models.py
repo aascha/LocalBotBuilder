99bd54266a6e530918b9d721d3b83a82686dad8e
raufhalila <abderraoufhalila@gmail.com>
2025-05-22 20:02:50 +0200

    Added a Users table to the database

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,4 +1,20 @@
 from . import db
+from werkzeug.security import generate_password_hash, check_password_hash
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    fullname = db.Column(db.String(120), nullable=False)
+    email = db.Column(db.String(120), nullable=False)
+    hashed_password = db.Column(db.String(128), nullable=False)
+
+    bots = db.relationship('BotHistory', backref='owner', lazy=True)
+
+
+    def set_password(self, password):
+        self.hashed_password = generate_password_hash(password)
+    
+    def check_password(self, password):
+        return check_password_hash(self.hashed_password, password)
 
 class BotHistory(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -11,3 +27,5 @@ class BotHistory(db.Model):
     indexed_file = db.Column(db.String(100))
     tone = db.Column(db.String(20))
     timestamp = db.Column(db.String(30))
+
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=True)
